239813c44829ddd09c46027fbd3e75b65431d4c3
Jeremy Magland <jeremy.magland@gmail.com>
2018-09-24 21:23:30 -0400

    add recording_to_h5 function

--- tools.py
+++ tools.py
@@ -9,6 +9,7 @@ import elephant
 import os
 from os.path import join
 import matplotlib.pylab as plt
+import h5py
 
 
 ### LOAD FUNCTIONS ###
@@ -1637,3 +1638,30 @@ def play_mea_recording(rec, mea_pos, mea_pitch, fs, window=1, step=0.1, colors=N
 
     return anim
 
+# Save recording to hdf5 file -- by jfm 9/24/2018
+def recording_to_hdf5(recording_folder,output_fname):
+    F=h5py.File(output_fname,'w')
+
+    with open(recording_folder+'/info.yaml', 'r') as f:
+        info = yaml.load(f)
+        
+    for key in info['General']:
+        F.attrs[key]=info['General'][key] # this includes spiketrain_folder, fs, template_folder, duration, n_neurons, seed, electrode_name
+
+    peaks=np.load(recording_folder+'/peaks.npy')
+    F.create_dataset('peaks',data=peaks)
+    positions=np.load(recording_folder+'/positions.npy')
+    F.create_dataset('positions',data=positions)
+    recordings=np.load(recording_folder+'/recordings.npy')
+    F.create_dataset('recordings',data=recordings)
+    sources=np.load(recording_folder+'/sources.npy')
+    F.create_dataset('sources',data=sources)
+    spiketrains=np.load(recording_folder+'/spiketrains.npy')
+    for ii in range(len(spiketrains)):
+        st=spiketrains[ii]
+        F.create_dataset('spiketrains/{}/times'.format(ii),data=st.times)
+    templates=np.load(recording_folder+'/templates.npy')
+    F.create_dataset('templates',data=templates)
+    templates=np.load(recording_folder+'/times.npy')
+    F.create_dataset('times',data=templates)
+    F.close()
