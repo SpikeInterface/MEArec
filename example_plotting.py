efc8485a30563d6cfe1e862a5d70f92ada0215f9
Alessio Buccino <alejoe9187@gmail.com>
2018-10-13 12:36:42 +0200

    updated example_plotting.py

--- example_plotting.py
+++ example_plotting.py
@@ -2,31 +2,27 @@ from __future__ import print_function
 
 '''Examples on how to load and plot templates, spike trains, recordings'''
 import matplotlib.pylab as plt
-from MEArec.MEArec import tools
+from MEArec import tools
 import MEAutility as MEA
 
-template_folder = None # insert here the template path
-spiketrain_folder = None # insert here the spiketrains path
-recording_folder = '/home/alessiob/Documents/Codes/MEArec/recordings/recordings_20cells_Neuronexus-32_10.0_10.0uV_25-09-2018:14:00' # insert here the recording path
+template_folder = '/home/alessio/Documents/Codes/MEArec/data/templates/physrot/templates_30_Neuronexus-32_13-10-2018' # insert here the template path
+recording_folder = '/home/alessio/Documents/Codes/MEArec/data/recordings/recordings_20cells_Neuronexus-32_10.0_10.0uV_13-10-2018:10:55' # insert here the recording path
 
 if template_folder is not None:
-    templates, locs, rots, celltypes, temp_info = tools.load_templates(template_folder)
-    electrode_name = temp_info['Electrodes']['electrode_name']
+    temp_dict, temp_info = tools.load_templates(template_folder)
+    electrode_name = temp_info['electrodes']['electrode_name']
     mea = MEA.return_mea(electrode_name)
 
-    MEA.plot_mea_recording(templates, mea)
+    MEA.plot_mea_recording(temp_dict['templates'][0], mea)
 
-if spiketrain_folder is not None:
-    spiketrains, st_info = tools.load_spiketrains(spiketrain_folder)
-
-    tools.raster_plots(spiketrains)
 
 if recording_folder is not None:
-    recordings, times, templates, positions, spiketrains, sources, peaks, rec_info = tools.load_recordings(recording_folder)
-    electrode_name = rec_info['General']['electrode_name']
+    rec_dict, rec_info = tools.load_recordings(recording_folder)
+    electrode_name = rec_info['recordings']['electrode_name']
     mea = MEA.return_mea(electrode_name)
-
-    MEA.plot_mea_recording(recordings, mea)
+    
+    MEA.plot_mea_recording(rec_dict['recordings'], mea)
+    tools.raster_plots(rec_dict['spiketrains'])
 
 plt.ion()
 plt.show()
