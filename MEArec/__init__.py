b3ce05e71404cc60fbc70974e4ca2c0b3bbe637b
Alessio Buccino <alejoe9187@gmail.com>
2018-10-14 21:41:26 +0200

    version 1.0.0

--- MEArec/__init__.py
+++ MEArec/__init__.py
@@ -1,3 +1,5 @@
 from MEArec.generators import gen_recordings, gen_templates
 from MEArec.tools import load_templates, load_recordings, hdf5_to_templates, \
     hdf5_to_recordings, templates_to_hdf5, recordings_to_hdf5
+
+__version__ = '1.0.0'
