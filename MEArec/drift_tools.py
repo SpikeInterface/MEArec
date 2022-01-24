cd7a545c04d6e9b69dabbe47627eb22ab53b27ed
Samuel Garcia <sam.garcia.die@gmail.com>
2022-01-24 20:46:00 +0100

    drift wip

--- MEArec/drift_tools.py
+++ MEArec/drift_tools.py
@@ -14,6 +14,7 @@ def generate_drift_position_vector(
     drift_mode_speed='slow',
 
     slow_drift_velocity=5,
+    slow_drift_waveform='triangluar',
     fast_drift_period=10.,
     fast_drift_max_jump=20.,
     fast_drift_min_jump=5.,
@@ -40,6 +41,8 @@ def generate_drift_position_vector(
     
     slow_drift_velocity: float
         drift velocity in um/min.
+    slow_drift_waveform: 'trianglar' or 'sine'
+        waveform for slow drift
     fast_drift_period: float
         period between fast drift events
     fast_drift_max_jump: float
@@ -57,28 +60,78 @@ def generate_drift_position_vector(
 
     num_cells, drift_steps, _ = template_locations.shape
     
-    # check on template_locs
+    # TODO check on template_locs
+    
+    
+    assert start_drift_index < n_samples, f' samples ({n_samples}) must be < start drift ({start_drift_index})'
+    
+    
+    
     
     if drift_mode_probe == 'rigid':
-        drift_vectors = np.zeros(n_samples, dtype='uint16')
+        #~ drift_vectors = np.zeros(n_samples, dtype='uint16')
+        drift_vectors = np.zeros(n_samples, dtype='float32')
     elif drift_mode_probe == 'non-rigid':
-        drift_vectors = np.zeros((n_samples, num_cells), dtype='uint16')
+        #~ drift_vectors = np.zeros((n_samples, num_cells), dtype='uint16')
+        drift_vectors = np.zeros((n_samples, num_cells), dtype='float32')
+        
+        
     
     # every cells start in the middle of drift vector line
     middle_step = drift_steps // 2
     drift_vectors[:] = middle_step
+
+    # velocity and jump are computed on bigger drift over cell
+    loc0 = template_locations[:, 0, :]
+    loc1 = template_locations[:, -1, :]
+    dist = np.sum((loc0 - loc1)**2, axis=1) ** 0.5
+    max_dist = np.max(dist)
+    max_step = max_dist /drift_steps
+
     
     if 'slow' in drift_mode_speed:
+        
+        # compute half period for a regular drift speed
+        half_period = max_dist / (slow_drift_velocity / 60)
+        
+        # trianle / sin ferquence
+        freq = 1. / (2 * half_period)
+        
         times = np.arange(n_samples - start_drift_index) / fs
         
-        # TODO make slow_drift_velocity to trangle frequency depending on the drift steps um
-        freq = 0.5
-        triangle = np.abs(scipy.signal.sawtooth(2 * np.pi * freq * times + np.pi / 2))
-        triangle *= (drift_steps - 1)
-        drift_vectors[start_drift_index:] = triangle
+        
+        if slow_drift_waveform == 'triangluar':
+            triangle = np.abs(scipy.signal.sawtooth(2 * np.pi * freq * times + np.pi / 2))
+            #~ triangle *= (drift_steps - 1)
+            drift_vectors[start_drift_index:] = triangle
+        elif slow_drift_waveform == 'sine':
+            sine = np.cos(2 * np.pi * freq * times + np.pi / 2)
+            sine += 1
+            sine *= (drift_steps - 1) / 2.
+            drift_vectors[start_drift_index:] = sine
+        else:
+            raise NotIMplementedError('slow_drift_waveform')
         
     if 'fast' in drift_mode_speed:
-        raise NotImplementedError
+        period = int(fast_drift_period * fs)
+        n = int(np.round((n_samples - start_drift_index) / period))
+        
+        pos = start_drift_index
+        for i in range(n):
+            jump = np.random.rand() * (fast_drift_max_jump - fast_drift_min_jump) + fast_drift_min_jump
+            if np.random.rand() > 0.5:
+                jump = -jump
+            jump_int = jump / max_step
+            
+            drift_vectors[pos: pos + period] += int(jump_int)
+            
+            drift_vectors[pos:] += int(jump_int)
+            pos += period
+    
+    # Alessio : how to handle the clipping
+    #~ drift_vectors
+    
+    drift_vectors = drift_vectors.astype('uint16')
     
     return drift_vectors
     
@@ -92,12 +145,28 @@ def test_generate_drift_position_vector():
     drift_steps=20
     template_locations = np.zeros((num_cells, drift_steps, 3))
     
+    # drift step 1.5 um of Z
+    template_locations[:, :, 2] = np.arange(drift_steps) * 1.5
+    #~ print(template_locations)
+    
     drift_vectors = generate_drift_position_vector(fs=fs,
         template_locations=template_locations,
-        n_samples=30000*20,
-        start_drift_index=30000,
+        n_samples=30000*600,
+        start_drift_index=30000* 5 ,
         drift_mode_probe='rigid',
-        drift_mode_speed='slow',
+        drift_mode_speed='slow+fast',
+        #~ drift_mode_speed='fast',
+        #~ drift_mode_speed='slow',
+        slow_drift_velocity=5.,
+        slow_drift_waveform='triangluar',
+        #~ slow_drift_velocity=3,
+        #~ slow_drift_waveform= 'sine',
+
+        fast_drift_period=120.,
+        fast_drift_max_jump=15.,
+        fast_drift_min_jump=2.,
+
+        
         )
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots()
