a576b0e5e7e6639638e2a41ac2434b81d9e0fe8e
Samuel Garcia <sam.garcia.die@gmail.com>
2023-02-23 11:04:45 +0100

    some fix to avoid memory blow

--- MEArec/generators/recordinggenerator.py
+++ MEArec/generators/recordinggenerator.py
@@ -790,11 +790,22 @@ class RecordingGenerator:
                     self.template_ids = reordered_idx_cells
                 else:
                     print(f"Using provided template ids: {self.template_ids}")
-                    template_celltypes = celltypes[self.template_ids]
-                    template_locs = np.array(locs)[self.template_ids]
-                    template_rots = np.array(rots)[self.template_ids]
-                    template_bin = np.array(bin_cat)[self.template_ids]
-                    templates = np.array(eaps)[self.template_ids]
+                    ordered_ids = np.all(np.diff(self.template_ids) > 0)
+                    if ordered_ids:
+                        template_celltypes = celltypes[self.template_ids]
+                        template_locs = np.array(locs[self.template_ids])
+                        template_rots = np.array(rots[self.template_ids])
+                        template_bin = np.array(bin_cat[self.template_ids])
+                        templates = np.array(eaps[self.template_ids])
+                    else:
+                        order = np.argsort(self.template_ids)
+                        order_back = np.argsort(order)
+                        sorted_ids = self.template_ids[order]
+                        template_celltypes = celltypes[sorted_ids][order_back]
+                        template_locs = np.array(locs[sorted_ids])[order_back]
+                        template_rots = np.array(rots[sorted_ids])[order_back]
+                        template_bin = np.array(bin_cat[sorted_ids])[order_back]
+                        templates = np.array(eaps[sorted_ids])[order_back]
                 
                 # compute gain
                 gain_to_int = None
